97768fd25fed5f46c443235e4ede95b3de4cfd45
Ermiyas Teklehaymanot <lilermi24@gmail.com>
2025-05-28 12:25:37 +0300

    feat: customize the django admin form

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,14 @@
 import datetime
 from django.utils import timezone
 from django.db import models
+from django.contrib import admin
 
 class Question(models.Model):
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
